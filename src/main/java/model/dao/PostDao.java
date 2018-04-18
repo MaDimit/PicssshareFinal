43ae2fd79f2e5d0ac9e1e443aa78cc979323f7cd
Maxim D <mdimirtov@gmail.com>
2018-04-18 12:54:03 +0300

    postDao like/dislike refactored

--- src/main/java/model/dao/PostDao.java
+++ src/main/java/model/dao/PostDao.java
@@ -41,24 +41,6 @@ public class PostDao extends Dao {
         return posts;
     }
 
-    public void addAlbumInDB(Album album) throws SQLException {
-        String sql = "INSERT INTO albums (name, belonger_id) VALUES (?,?)";
-        PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        stmt.setString(1,album.getName());
-        stmt.setInt(2,album.getUserID());
-        stmt.executeUpdate();
-
-        ResultSet generatedKeys = stmt.getGeneratedKeys();
-        int id;
-        if (generatedKeys.next()) {
-            id = generatedKeys.getInt(1);
-            album.setId(id);
-        } else {
-            throw new SQLException("Creating post failed, no ID obtained.");
-        }
-        stmt.close();
-    }
-
     //method for fill the collection with posts and all the information about them
     public void loadAllPosts() throws SQLException {
         String sql = "SELECT id,date,poster_id,url FROM posts";
@@ -195,6 +177,25 @@ public class PostDao extends Dao {
 
     public void dislikeComment(Comment comment) throws SQLException{
         //TODO comment disliking
+    }
+
+    //------------------ albums manipulations ------------------//
+    public void addAlbumInDB(Album album) throws SQLException {
+        String sql = "INSERT INTO albums (name, belonger_id) VALUES (?,?)";
+        PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1,album.getName());
+        stmt.setInt(2,album.getUserID());
+        stmt.executeUpdate();
+
+        ResultSet generatedKeys = stmt.getGeneratedKeys();
+        int id;
+        if (generatedKeys.next()) {
+            id = generatedKeys.getInt(1);
+            album.setId(id);
+        } else {
+            throw new SQLException("Creating post failed, no ID obtained.");
+        }
+        stmt.close();
     }
 
     //================== Post likes/dislikes ==================//
